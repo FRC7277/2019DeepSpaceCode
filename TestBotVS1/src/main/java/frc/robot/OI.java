eccde80d5173a3bc69c3526e33fad997d861e93f
Dat Lam <d4nnyl4m@gmail.com>
2018-12-12 15:58:28 -0700

    testing

--- TestBotVS1/src/main/java/frc/robot/OI.java
+++ TestBotVS1/src/main/java/frc/robot/OI.java
@@ -12,7 +12,6 @@ package frc.robot;
  * interface to the commands and command groups that allow control of the robot.
  */
 
- //test 4
 public class OI {
   //// CREATING BUTTONS
   // One type of button is a joystick button which is any button on a
