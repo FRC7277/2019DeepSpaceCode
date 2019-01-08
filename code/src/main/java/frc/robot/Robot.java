4b37dcb59ef5c37cc228c3fe7ad994a357b08291
HN67 <hn67allardkids@gmail.com>
2019-01-07 18:37:24 -0600

    Changed camera to be processed

--- code/src/main/java/frc/robot/Robot.java
+++ code/src/main/java/frc/robot/Robot.java
@@ -16,6 +16,12 @@ import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.ExampleSubsystem;
 
 import edu.wpi.first.cameraserver.CameraServer;
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
+
+import edu.wpi.cscore.CvSink;
+import edu.wpi.cscore.CvSource;
+import edu.wpi.cscore.UsbCamera;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -41,7 +47,28 @@ public class Robot extends TimedRobot {
     m_chooser.setDefaultOption("Default Auto", new ExampleCommand());
     // chooser.addOption("My Auto", new MyAutoCommand());
     SmartDashboard.putData("Auto mode", m_chooser);
-    CameraServer.getInstance().startAutomaticCapture(0);
+    // Put camera to dashboard
+    //CameraServer.getInstance().startAutomaticCapture(0);
+
+    //Create thread for processing camera (asynchrous)
+    new Thread(() -> {
+      UsbCamera camera = CameraServer.getInstance().startAutomaticCapture();
+      camera.setResolution(8, 6);
+      
+      CvSink cvSink = CameraServer.getInstance().getVideo();
+      CvSource outputStream = CameraServer.getInstance().putVideo("Blur", 8, 6);
+      
+      Mat source = new Mat();
+      Mat output = new Mat();
+      
+      while(!Thread.interrupted()) {
+          cvSink.grabFrame(source);
+          Imgproc.cvtColor(source, output, Imgproc.COLOR_BGR2GRAY);
+          outputStream.putFrame(output);
+
+          System.out.println("Dab");
+      }
+      }).start();
   }
 
   /**
