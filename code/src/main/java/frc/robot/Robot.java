ecc32da45531a466691eec68a6e1a8876f79f4f9
HN67 <hn67allardkids@gmail.com>
2019-01-08 16:44:16 -0600

    Fixed camera processing

--- code/src/main/java/frc/robot/Robot.java
+++ code/src/main/java/frc/robot/Robot.java
@@ -31,144 +31,169 @@ import edu.wpi.cscore.UsbCamera;
  * project.
  */
 public class Robot extends TimedRobot {
-  public static ExampleSubsystem m_subsystem = new ExampleSubsystem();
-  public static OI m_oi;
+    public static ExampleSubsystem m_subsystem = new ExampleSubsystem();
+    public static OI m_oi;
 
-  Command m_autonomousCommand;
-  SendableChooser<Command> m_chooser = new SendableChooser<>();
+    Command m_autonomousCommand;
+    SendableChooser<Command> m_chooser = new SendableChooser<>();
 
-  /**
-   * This function is run when the robot is first started up and should be
-   * used for any initialization code.
-   */
-  @Override
-  public void robotInit() {
-    m_oi = new OI();
-    m_chooser.setDefaultOption("Default Auto", new ExampleCommand());
-    // chooser.addOption("My Auto", new MyAutoCommand());
-    SmartDashboard.putData("Auto mode", m_chooser);
-    // Put camera to dashboard
-    // CameraServer.getInstance().startAutomaticCapture(0);
+    SendableChooser<String> vc_chooser = new SendableChooser<>();
 
-    // Create thread for processing camera (asynchrous)
-    new Thread(() -> {
-
-      // Reference USB camera connected to RIO (and also start automatic capture)
-      UsbCamera camera = CameraServer.getInstance().startAutomaticCapture(0);
-      // Define resolution for the camera
-      camera.setResolution(320, 240);
-      
-      // Create a CvSink pulling from the camera
-      CvSink cvSink = CameraServer.getInstance().getVideo();
-      // Create cv output linked to SmartDashboard component 'Gray'
-      CvSource outputStream = CameraServer.getInstance().putVideo("Gray", 320, 240);
-      
-      // Create mats (matrice capable of containing images) for source and output
-      // Source stores a raw frame from cvSink
-      Mat source = new Mat();
-      // Output provides container to put processed frame in
-      Mat output = new Mat();
-      
-      // While loop processes until the thread is interrupted
-      while(!Thread.interrupted()) {
-          //Frame is taken from cvSink and put into source
-          cvSink.grabFrame(source);
-          // Source is proccessed, and the result is put in output
-          // Current processing is turning to grayscale (mainly for testing)
-          Imgproc.cvtColor(source, output, Imgproc.COLOR_BGR2GRAY);
-          // Output is pushed into the output stream (linked to dashboard)
-          outputStream.putFrame(output);
-
-      }
-
-      }).start();
-  }
-
-  /**
-   * This function is called every robot packet, no matter the mode. Use
-   * this for items like diagnostics that you want ran during disabled,
-   * autonomous, teleoperated and test.
-   *
-   * <p>This runs after the mode specific periodic functions, but before
-   * LiveWindow and SmartDashboard integrated updating.
-   */
-  @Override
-  public void robotPeriodic() {
-  }
-
-  /**
-   * This function is called once each time the robot enters Disabled mode.
-   * You can use it to reset any subsystem information you want to clear when
-   * the robot is disabled.
-   */
-  @Override
-  public void disabledInit() {
-  }
-
-  @Override
-  public void disabledPeriodic() {
-    Scheduler.getInstance().run();
-  }
-
-  /**
-   * This autonomous (along with the chooser code above) shows how to select
-   * between different autonomous modes using the dashboard. The sendable
-   * chooser code works with the Java SmartDashboard. If you prefer the
-   * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-   * getString code to get the auto name from the text box below the Gyro
-   *
-   * <p>You can add additional auto modes by adding additional commands to the
-   * chooser code above (like the commented example) or additional comparisons
-   * to the switch structure below with additional strings & commands.
-   */
-  @Override
-  public void autonomousInit() {
-    m_autonomousCommand = m_chooser.getSelected();
-
-    /*
-     * String autoSelected = SmartDashboard.getString("Auto Selector",
-     * "Default"); switch(autoSelected) { case "My Auto": autonomousCommand
-     * = new MyAutoCommand(); break; case "Default Auto": default:
-     * autonomousCommand = new ExampleCommand(); break; }
+    /**
+     * This function is run when the robot is first started up and should be used
+     * for any initialization code.
      */
+    @Override
+    public void robotInit() {
+        m_oi = new OI();
+        // Setup auto chooser
+        m_chooser.setDefaultOption("Default Auto", new ExampleCommand());
+        m_chooser.addOption("Fancy Auto", new ExampleCommand());
+        // chooser.addOption("My Auto", new MyAutoCommand());
+        SmartDashboard.putData("Auto mode", m_chooser);
 
-    // schedule the autonomous command (example)
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.start();
+        // Setup camera chooser
+        vc_chooser.setDefaultOption("Normal", "USB Camera 0");
+        vc_chooser.addOption("Gray", "Gray");
+        SmartDashboard.putData("Camera mode", vc_chooser);
+
+        // Put camera to dashboard
+        // CameraServer.getInstance().startAutomaticCapture(0);
+
+        // Create thread for processing camera (asynchrous)
+        new Thread(() -> {
+
+            // Reference USB camera connected to RIO (and also start automatic capture)
+            UsbCamera camera = CameraServer.getInstance().startAutomaticCapture();
+            // Define resolution for the camera
+            camera.setResolution(640, 480);
+
+            // System.out.println("#&# Referenced camera "+camera);
+
+            // Create a CvSink pulling from the camera
+            CvSink cvSink = CameraServer.getInstance().getVideo();
+            // Create cv output linked to SmartDashboard component 'Gray'
+            CvSource outputStream = CameraServer.getInstance().putVideo("Gray", 640, 480);
+            
+            // Create misc output
+            CvSource miscOutput = CameraServer.getInstance().putVideo("Misc", 640, 480);
+
+            // Create mats (matrice capable of containing images) for source and output
+            // Source stores a raw frame from cvSink
+            Mat source = new Mat();
+            // Output provides container to put processed frame in
+            Mat output = new Mat();
+
+            // While loop processes until the thread is interrupted
+            while (!Thread.interrupted()) {
+                // Frame is taken from cvSink and put into source
+                if (cvSink.grabFrame(source) != 0) {
+                    // Source is proccessed, and the result is put in output
+                    // Current processing is turning to grayscale (mainly for testing)
+                    Imgproc.cvtColor(source, output, Imgproc.COLOR_BGR2GRAY);
+                    // Output is pushed into the output stream (linked to dashboard)
+                    outputStream.putFrame(output);
+
+                    //Ouput misc
+                    if (vc_chooser.getSelected().equals("Gray")) {
+                        miscOutput.putFrame(output);
+                    } else {
+                        miscOutput.putFrame(source);
+                    }
+                }
+
+            }
+
+        }).start();
     }
-  }
 
-  /**
-   * This function is called periodically during autonomous.
-   */
-  @Override
-  public void autonomousPeriodic() {
-    Scheduler.getInstance().run();
-  }
-
-  @Override
-  public void teleopInit() {
-    // This makes sure that the autonomous stops running when
-    // teleop starts running. If you want the autonomous to
-    // continue until interrupted by another command, remove
-    // this line or comment it out.
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.cancel();
+    /**
+     * This function is called every robot packet, no matter the mode. Use this for
+     * items like diagnostics that you want ran during disabled, autonomous,
+     * teleoperated and test.
+     *
+     * <p>
+     * This runs after the mode specific periodic functions, but before LiveWindow
+     * and SmartDashboard integrated updating.
+     */
+    @Override
+    public void robotPeriodic() {
     }
-  }
 
-  /**
-   * This function is called periodically during operator control.
-   */
-  @Override
-  public void teleopPeriodic() {
-    Scheduler.getInstance().run();
-  }
+    /**
+     * This function is called once each time the robot enters Disabled mode. You
+     * can use it to reset any subsystem information you want to clear when the
+     * robot is disabled.
+     */
+    @Override
+    public void disabledInit() {
+    }
 
-  /**
-   * This function is called periodically during test mode.
-   */
-  @Override
-  public void testPeriodic() {
-  }
+    @Override
+    public void disabledPeriodic() {
+        Scheduler.getInstance().run();
+    }
+
+    /**
+     * This autonomous (along with the chooser code above) shows how to select
+     * between different autonomous modes using the dashboard. The sendable chooser
+     * code works with the Java SmartDashboard. If you prefer the LabVIEW Dashboard,
+     * remove all of the chooser code and uncomment the getString code to get the
+     * auto name from the text box below the Gyro
+     *
+     * <p>
+     * You can add additional auto modes by adding additional commands to the
+     * chooser code above (like the commented example) or additional comparisons to
+     * the switch structure below with additional strings & commands.
+     */
+    @Override
+    public void autonomousInit() {
+        m_autonomousCommand = m_chooser.getSelected();
+
+        /*
+         * String autoSelected = SmartDashboard.getString("Auto Selector", "Default");
+         * switch(autoSelected) { case "My Auto": autonomousCommand = new
+         * MyAutoCommand(); break; case "Default Auto": default: autonomousCommand = new
+         * ExampleCommand(); break; }
+         */
+
+        // schedule the autonomous command (example)
+        if (m_autonomousCommand != null) {
+            m_autonomousCommand.start();
+        }
+    }
+
+    /**
+     * This function is called periodically during autonomous.
+     */
+    @Override
+    public void autonomousPeriodic() {
+        Scheduler.getInstance().run();
+    }
+
+    @Override
+    public void teleopInit() {
+        // This makes sure that the autonomous stops running when
+        // teleop starts running. If you want the autonomous to
+        // continue until interrupted by another command, remove
+        // this line or comment it out.
+        if (m_autonomousCommand != null) {
+            m_autonomousCommand.cancel();
+        }
+    }
+
+    /**
+     * This function is called periodically during operator control.
+     */
+    @Override
+    public void teleopPeriodic() {
+        Scheduler.getInstance().run();
+    }
+
+    /**
+     * This function is called periodically during test mode.
+     */
+    @Override
+    public void testPeriodic() {
+    }
 }
