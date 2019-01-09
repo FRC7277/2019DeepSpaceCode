73a19ff56bcf99154dd9865108c50e12bfc48dfe
HN67 <31172532+HN67@users.noreply.github.com>
2019-01-09 10:06:43 -0700

    Create joystick object in OI

--- code/src/main/java/frc/robot/OI.java
+++ code/src/main/java/frc/robot/OI.java
@@ -7,6 +7,9 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.Joystick;
+import frc.robot.RobotMap;
+
 /**
  * This class is the glue that binds the controls on the physical operator
  * interface to the commands and command groups that allow control of the robot.
@@ -39,4 +42,7 @@ public class OI {
   // Start the command when the button is released and let it run the command
   // until it is finished as determined by it's isFinished method.
   // button.whenReleased(new ExampleCommand());
+  
+  public Joystick joystick = new Joystick(RobotMap.joystick);
+  
 }
