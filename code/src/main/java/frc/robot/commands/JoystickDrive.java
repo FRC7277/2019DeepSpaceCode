06b5872f432c1c51fc1f7552d80dc87692f49caa
HN67 <31172532+HN67@users.noreply.github.com>
2019-01-09 09:58:30 -0700

    Create Joystick Drive
    Drive command that takes Joystick object and arcade drives Robot.m_drivetrain

--- code/src/main/java/frc/robot/commands/JoystickDrive.java
+++ code/src/main/java/frc/robot/commands/JoystickDrive.java
@@ -8,15 +8,24 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.Joystick;
+
 import frc.robot.Robot;
 
 /**
  * An example command.  You can replace me with your own command.
  */
-public class MainDrive extends Command {
-  public MainDrive() {
+public class JoystickDrive extends Command {
+  
+  Joystick joy;
+  
+  public JoystickDrive(Joystick joy) {
     // Use requires() here to declare subsystem dependencies
     requires(Robot.m_drivetrain);
+    
+    // Reference passed joystick
+    this.joy = joy
+    
   }
 
   // Called just before this Command runs the first time
@@ -27,6 +36,9 @@ public class MainDrive extends Command {
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
+    
+    Robot.m_drivetrain.getDrive().arcadeDrive(-this.joy.getY(), this.joy.getX());
+    
   }
 
   // Make this return true when this Command no longer needs to run execute()
@@ -38,11 +50,13 @@ public class MainDrive extends Command {
   // Called once after isFinished returns true
   @Override
   protected void end() {
+    Robot.m_drivetrain.getDrive().arcadeDrive(0, 0);
   }
 
   // Called when another command which requires one or more of the same
   // subsystems is scheduled to run
   @Override
   protected void interrupted() {
+    this.end();
   }
 }
