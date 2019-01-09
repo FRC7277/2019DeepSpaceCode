f1657d243395e516a3eb3d89b86a9f277ebb6c9b
HN67 <31172532+HN67@users.noreply.github.com>
2019-01-09 09:43:21 -0700

    Improved scoping and added DI (int based)

--- code/src/main/java/frc/robot/subsystems/DriveTrain.java
+++ code/src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -19,18 +19,57 @@ import frc.robot.RobotMap;
 public class DriveTrain extends Subsystem {
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
-    WPI_TalonSRX leftBack = new WPI_TalonSRX(RobotMap.leftBack);
-    WPI_TalonSRX rightBack = new WPI_TalonSRX(RobotMap.rightBack);
-    WPI_TalonSRX leftFront = new WPI_TalonSRX(RobotMap.leftFront);
-    WPI_TalonSRX rightFront = new WPI_TalonSRX(RobotMap.rightFront);
-    SpeedControllerGroup left = new SpeedControllerGroup(leftBack, leftFront);
-    SpeedControllerGroup right = new SpeedControllerGroup(rightBack, rightFront);
+    
+    // Define motor object for each motor
+    private WPI_TalonSRX leftBack;
+    private WPI_TalonSRX rightBack;
+    private WPI_TalonSRX leftFront;
+    private WPI_TalonSRX rightFront;
+    
+    // Define left and right speedcontroller groups
+    private SpeedControllerGroup left;
+    private SpeedControllerGroup right;
+    
+    // Define differential drive
     private DifferentialDrive m_drive;
-
-    public DriveTrain () {
-      m_drive = new DifferentialDrive(left, right);
+    
+    /**
+     * Takes 4 port integers, left-back, right-back, left-front, right-front
+     */
+    public DriveTrain(int lb, int rb, int lf, int rf) {
+        
+        // Create Motor objects using parameters
+        this.leftBack = new WPI_TalonSRX(lb);
+        this.rightBack = new WPI_TalonSRX(rb);
+        this.leftFront = new WPI_TalonSRX(lf);
+        this.rightFront = new WPI_TalonSRX(rf);
+        
+        // Create groups of controllers
+        this.left = new SpeedControllerGroup(this.leftBack, this.leftFront);
+        this.right = new SpeedControllerGroup(this.rightBack, this.rightFront);
+        
+        // Create diff drive
+        this.m_drive = new DifferentialDrive(left, right);
+        
+    }
+    
+    /**
+     * Constructs a drivetrain using RobotMap
+     */
+    public DriveTrain() {
+        
+        // Pass robotmap into DI constructor
+        this(RobotMap.leftBack, RobotMap.rightBack, RobotMap.leftFront, RobotMap.rightFront);
 
     }
+    
+    /**
+     * Accessor for the differential drive
+     */
+    public DifferentialDrive getDrive() {
+      return this.m_drive;   
+    }
+    
     @Override
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
