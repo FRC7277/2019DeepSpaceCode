1b820914b2117c2d5b38056ec9f571dfb1d5ba56
HN67 <31172532+HN67@users.noreply.github.com>
2019-01-18 09:45:29 -0700

    Cleaned robot of dead code

--- code/src/main/java/frc/robot/Robot.java
+++ code/src/main/java/frc/robot/Robot.java
@@ -53,10 +53,8 @@ public class Robot extends TimedRobot {
     public static DriveTrain m_drivetrain = new DriveTrain();
     public static ElevatorLift elevator = new ElevatorLift();
     
-    Command m_autonomousCommand;
-    // Create dashboard choosers
-    SendableChooser<Command> m_chooser = new SendableChooser<>();
-    SendableChooser<String> vc_chooser = new SendableChooser<>();
+    // Create dashboard camera chooser
+    private SendableChooser<String> vc_chooser = new SendableChooser<>();
 
     /**
      * This function is run when the robot is first started up and should be used
@@ -64,18 +62,11 @@ public class Robot extends TimedRobot {
      */
     @Override
     public void robotInit() {
-        // Setup auto chooser
-        //m_chooser.setDefaultOption("Default Auto", new ExampleCommand());
-        // chooser.addOption("My Auto", new MyAutoCommand());
-        SmartDashboard.putData("Auto mode", m_chooser);
 
         // Setup camera chooser
         vc_chooser.setDefaultOption("Normal", "USB Camera 0");
         vc_chooser.addOption("Gray", "Gray");
         SmartDashboard.putData("Camera mode", vc_chooser);
-
-        // Put camera to dashboard
-        // CameraServer.getInstance().startAutomaticCapture(0);
 
         // Create thread for processing camera (asynchrous)
         new Thread(() -> {
@@ -170,19 +161,7 @@ public class Robot extends TimedRobot {
      */
     @Override
     public void autonomousInit() {
-        m_autonomousCommand = m_chooser.getSelected();
 
-        /*
-         * String autoSelected = SmartDashboard.getString("Auto Selector", "Default");
-         * switch(autoSelected) { case "My Auto": autonomousCommand = new
-         * MyAutoCommand(); break; case "Default Auto": default: autonomousCommand = new
-         * ExampleCommand(); break; }
-         */
-
-        // schedule the autonomous command (example)
-        if (m_autonomousCommand != null) {
-            m_autonomousCommand.start();
-        }
     }
 
     /**
@@ -195,13 +174,7 @@ public class Robot extends TimedRobot {
 
     @Override
     public void teleopInit() {
-        // This makes sure that the autonomous stops running when
-        // teleop starts running. If you want the autonomous to
-        // continue until interrupted by another command, remove
-        // this line or comment it out.
-        if (m_autonomousCommand != null) {
-            m_autonomousCommand.cancel();
-        }
+        //Remember to cancel an autonomous command if we run one
     }
 
     /**
