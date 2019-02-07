4c0a85c93cbe11847929636008c97fc9932af207
HN67 <hn67allardkids@gmail.com>
2019-02-07 12:29:16 -0700

    Warning cleanup

--- code/src/main/java/frc/robot/commands/elevator/ControlElevator.java
+++ code/src/main/java/frc/robot/commands/elevator/ControlElevator.java
@@ -7,7 +7,6 @@
 
 package frc.robot.commands.elevator;
 
-import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 import edu.wpi.first.wpilibj.XboxController;
--- code/src/main/java/frc/robot/commands/elevator/MoveElevator.java
+++ code/src/main/java/frc/robot/commands/elevator/MoveElevator.java
@@ -7,7 +7,6 @@
 
 package frc.robot.commands.elevator;
 
-import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.DigitalInput;
 
 import frc.robot.Robot;
